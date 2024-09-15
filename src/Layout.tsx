b0c91b8a18125958bd61102e5ec03152358f4d9f
EggLovej <egglovej@gmail.com>
2024-09-15 16:30:42 +0200

    add: Speedinsights on Vercel

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,11 +2,13 @@ import type { ReactNode } from "react";
 import { Layout as RALayout, CheckForApplicationUpdate } from "react-admin";
 import CustomMenu from "./CustomMenu";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const Layout = ({ children }: { children: ReactNode }) => (
   <RALayout menu={CustomMenu}>
     {children}
     <CheckForApplicationUpdate />
+    <SpeedInsights />
     <Analytics />
   </RALayout>
 );
