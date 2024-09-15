faca46a73dcdc8cbd9bb272382e490a355efac03
EggLovej <egglovej@gmail.com>
2024-09-15 16:27:30 +0200

    add: Added web analytics for Vercel

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from "react";
 import { Layout as RALayout, CheckForApplicationUpdate } from "react-admin";
 import CustomMenu from "./CustomMenu";
+import { Analytics } from "@vercel/analytics/react"
 
 export const Layout = ({ children }: { children: ReactNode }) => (
   <RALayout menu={CustomMenu}>
     {children}
     <CheckForApplicationUpdate />
+    <Analytics />
   </RALayout>
 );
